ee19db2d2fe0c9fb50055a1310d28fc5c9362348
Allan Gabriel Ramos da Silva <allan.silva@rain.us>
2022-07-25 23:42:32 -0300

    feat(cors): add cors lib

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
