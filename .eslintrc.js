a2717b61f9dcbcd4c2bfd32e5c6772bc3d583d8f
Allan Gabriel Ramos da Silva <allan.silva@rain.us>
2022-07-25 23:38:12 -0300

    chore(eslint): add more packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,25 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+    'sort-destructure-keys',
+  ],
   rules: {
     semi: ['error', 'never'],
+    "no-console": 1,
+    "sort-destructure-keys/sort-destructure-keys": 2,
   },
 }
