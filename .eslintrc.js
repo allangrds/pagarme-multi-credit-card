a2fcc5549c9588d87adb320ccab434a3caff28ee
Allan Gabriel Ramos da Silva <allan.silva@rain.us>
2022-07-25 23:18:01 -0300

    chore(prettier): add tool to format code with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
-  "env": {
-    "es2021": true,
-    "node": true,
-    "jest": true
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "rules": {
-    "semi": ["error", "never"]
-  }
+  plugins: ['@typescript-eslint'],
+  rules: {
+    semi: ['error', 'never'],
+  },
 }
